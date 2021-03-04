620d2e85988d66370e0cfc333ef464a8008aff81
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-03-04 12:12:27 +0100

    Working "Clinical" Beacon client.

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
-# varfish-clinical-beacon-client
+# Clinical Beacon API Client
+
+Except for authentication uses official GA4GH Beacon protocol.
+
+- https://github.com/ga4gh-beacon/specification
+
+To facilitate controlled access without the burden of implementing OAuth, the upcoming IETF standard "Signing HTTP Messages" is employed:
+
+- https://tools.ietf.org/html/draft-ietf-httpbis-message-signatures-01
+
+The assumption is that two trusted sites are connected and both the querying and the responding site trust each other.
+
+Briefly, you have to specify:
+
+- `X-Beacon-User` HTTP header
+- `Authorization` HTTP header with `Signature` as described in the IETF standard draft.
+  The headers `date` and `x-beacon-user` must be signed.
+
+The Python code in this repository demonstrates how to do this.
+
+## Installation
+
+1. Setup virtualenv or conda environment (your choice)
+2. Clone, install dependencies, test
+    ```bash
+    $ git clone git@github.com:bihealth/varfish-clinical-beacon-client.git
+    $ cd varfish-clinical-beacon-client
+    $ pip install -r requirements.txt
+    ```
+3. Test
+    ```bash
+    $ python clinical-beacon-client.py --help
+    ```
+
+## Generate Key
+
+```bash
+$ ./clinical-beacon-client.py gen-key
+[I 210304 11:49:49 clinical-beacon-client:14] Generating public/private key pair...
+[I 210304 11:49:49 clinical-beacon-client:15]   public key:  key_rsa
+[I 210304 11:49:49 clinical-beacon-client:21]   private key: key_rsa.pub
+[I 210304 11:49:49 clinical-beacon-client:26] ... done generating key pair.
+[I 210304 11:49:49 clinical-beacon-client:27] Have a nice day!
+$ ls key_rsa*
+key_rsa  key_rsa.pub
+```
+
+## Register Key
+
+Now you have to register the public key in `key_rsa.pub` with the remote that you want to query.
+From this, you obtain a key id, say `test-client`.
+You can now contact the beacon.
+
+## Get Beacon Info
+
+```bash
+$ ./clinical-beacon-client.py query \
+    --key-id test-client \
+    --beacon-user testuser@example.com \
+    --endpoint https://beacon.example.com/endpoint/
+[I 210304 12:09:39 clinical-beacon-client:47] Asking beacon for info...
+[I 210304 12:09:39 clinical-beacon-client:69] => OK 200
+[I 210304 12:09:39 clinical-beacon-client:70] result below on stdout --8<-- --8<-- --8<--
+{
+  "id": "com.example.beacon",
+  "name": "Example Beacon Site",
+  "apiVersion": "v1.0.0",
+  "organisation": {
+    "id": "com.example",
+    "name": "Example Org",
+    "description": "Just for example..."
+  },
+  "datasets": [
+    {
+      "id": "com.example.beacon.ds-1",
+      "name": "Dataset",
+      "assembly": "GRCh37",
+      "description": null
+    }
+  ]
+}
+[I 210304 12:09:39 clinical-beacon-client:72] -->8-- -->8-- -->8-- result above on stdout
+[I 210304 12:09:39 clinical-beacon-client:86] ... done asking beacon for info.
+[I 210304 12:09:39 clinical-beacon-client:87] Have a nice day!
+```
+
+## Perform Query
+
+```bash
+$ ./clinical-beacon-client.py query \
+    --key-id test-client \
+    --beacon-user testuser@example.com \
+    --endpoint https://beacon.example.com/endpoint/ \
+    --variant 1-16977-G-A
+[I 210304 12:07:45 clinical-beacon-client:36] Executing query for 1-16977-G-A...
+[I 210304 12:07:47 clinical-beacon-client:69] => OK 200
+[I 210304 12:07:47 clinical-beacon-client:70] result below on stdout --8<-- --8<-- --8<--
+{
+  "beaconId": "com.example.beacon",
+  "apiVersion": "v1.0.0",
+  "exists": true,
+  "alleleRequest": {
+    "referenceName": "1",
+    "referenceBases": "G",
+    "start": 16977,
+    "alternateBases": "A",
+    "assemblyId": "GRCh37"
+  },
+  "datasetAlleleResponse": null,
+  "error": null
+}
+[I 210304 12:07:47 clinical-beacon-client:72] -->8-- -->8-- -->8-- result above on stdout
+[I 210304 12:07:47 clinical-beacon-client:84] ... done executing query.
+[I 210304 12:07:47 clinical-beacon-client:87] Have a nice day!
+```
---
+++ clinical-beacon-client.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import json
+import sys
+
+from Crypto.PublicKey import RSA
+import logzero
+from logzero import logger
+import requests
+from requests_http_signature import HTTPSignatureAuth
+
+
+def run_gen_key(args):
+    logger.info("Generating public/private key pair...")
+    logger.info("  public key:  %s", args.key_file)
+
+    key = RSA.generate(args.key_length)
+    with open(args.key_file, "wb") as f:
+        f.write(key.export_key("PEM"))
+
+    logger.info("  private key: %s.pub", args.key_file)
+
+    with open("%s.pub" % args.key_file, "wb") as f:
+        f.write(key.public_key().export_key("PEM"))
+
+    logger.info("... done generating key pair.")
+    logger.info("Have a nice day!")
+
+
+def run_query(args):
+    while args.endpoint.endswith("/"):
+        args.endpoint = args.endpoint[:-1]
+    if args.variant:
+        logger.info("Executing query for %s...", args.variant)
+        url = "%s/query" % args.endpoint
+        chrom, pos, ref, alt = args.variant.split("-")
+        params = {
+            "assemblyId": "GRCh37",
+            "referenceName": chrom,
+            "start": pos,
+            "referenceBases": ref,
+            "alternateBases": alt,
+        }
+    else:
+        logger.info("Asking beacon for info...")
+        url = "%s/" % args.endpoint
+        params = None
+
+    with open(args.key_file, "rb") as keyf:
+        key = keyf.read()
+
+    headers = {"X-Beacon-User": args.beacon_user}
+
+    r = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        auth=HTTPSignatureAuth(
+            algorithm="rsa-sha256",
+            key=key,
+            key_id=args.key_id,
+            headers=["date", "x-beacon-user"],
+        ),
+    )
+
+    if r.ok:
+        logger.info("=> OK %s", r.status_code)
+        logger.info("result below on stdout --8<-- --8<-- --8<--")
+        print(json.dumps(r.json(), indent=2))
+        logger.info("-->8-- -->8-- -->8-- result above on stdout")
+    else:
+        logger.info("=> ERROR: %s (%s)", r.status_code, r.reason)
+        try:
+            data = json.dumps(r.json(), indent=2)
+            logger.info("details below on stdout --8<-- --8<-- --8<--")
+            print(data)
+            logger.info("-->8-- -->8-- -->8-- details above on stdout")
+        except:
+            pass  # swallow
+
+    if args.variant:
+        logger.info("... done executing query.")
+    else:
+        logger.info("... done asking beacon for info.")
+    logger.info("Have a nice day!")
+
+
+def main(argv=None):
+    logzero.loglevel(logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Clinical Beacon Client")
+    subparsers = parser.add_subparsers(title="subcommands")
+
+    parser_gen_key = subparsers.add_parser(
+        "gen-key", help="Create public/private key pair"
+    )
+    parser_gen_key.set_defaults(func=run_gen_key)
+    parser_gen_key.add_argument(
+        "--key-length",
+        type=int,
+        default=2048,
+        help="Optional key length to set.",
+    )
+    parser_gen_key.add_argument(
+        "--key-file", help="Path to private key output file", default="key_rsa"
+    )
+
+    parser_query = subparsers.add_parser("query", help="Run query")
+    parser_query.set_defaults(func=run_query)
+    parser_query.add_argument(
+        "--key-file", help="Path to private key output file", default="key_rsa"
+    )
+    parser_query.add_argument(
+        "--key-id",
+        help="Name of the source site to end as (keyId).",
+        required=True,
+    )
+    parser_query.add_argument(
+        "--beacon-user",
+        help="String to use as the X-Beacon-User header value.",
+        required=True,
+    )
+    parser_query.add_argument(
+        "--endpoint", help="Remote endpoint URL.", required=True
+    )
+    parser_query.add_argument(
+        "--variant",
+        help=(
+            "Variant as CHROM-POS-REF-ALT.  If not specified then "
+            "the beacon information is queried for."
+        ),
+    )
+    parser_query.set_defaults(func=run_query)
+
+    args = parser.parse_args((argv or [])[1:])
+    if not hasattr(args, "func"):
+        parser.print_help()
+        return 1
+    else:
+        args.func(args)
+        return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
